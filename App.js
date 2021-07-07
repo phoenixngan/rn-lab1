02d118b115b4d1b4a20c552cab187bf60006300b
Phoenix Ngan <phoenix@Phoenixs-MacBook-Pro.local>
2021-07-07 13:46:28 -0700

    testing

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ export default function App() {
         <Image source={require("./assets/perry.png")} style={styles.photo} />
       </View>
       <View style={styles.nameContainer}>
-        <Text style={styles.nameAndPronouns}>Perry (she/her)</Text>
+        <Text style={styles.nameAndPronouns}>Perry!!! (she/her)</Text>
       </View>
       <View style={styles.blurbContainer}>
         <Text style={styles.blurb}>chirp chirp i'm a parakeet</Text>
